38a902f061f0f96a3ea098d2cbe6f89c24e3a921
raissaputi <raissa.puti@gmail.com>
2025-02-14 19:48:05 +0700

    delete comments

--- ECommerceApplication/src/main/java/com/app/controllers/BrandController.java
+++ ECommerceApplication/src/main/java/com/app/controllers/BrandController.java
@@ -28,17 +28,6 @@ public class BrandController {
         return new ResponseEntity<>(savedBrandDTO, HttpStatus.CREATED);
     }
 
-    // @GetMapping("/public/brands")
-    // public ResponseEntity<BrandResponse> getBrands(
-    //         @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
-    //         @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
-    //         @RequestParam(name = "sortBy", defaultValue = "name", required = false) String sortBy,
-    //         @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
-
-    //     BrandResponse brandResponse = brandService.getBrands(pageNumber, pageSize, sortBy, sortOrder);
-    //     return new ResponseEntity<>(brandResponse, HttpStatus.OK);
-    // }
-
     @GetMapping("/public/brands")
     public ResponseEntity<BrandResponse> getBrands(
         @RequestParam(name = "pageNumber", defaultValue = "0") Integer pageNumber,
